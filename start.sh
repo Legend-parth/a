471ac59211d1cab08e504a340c6afa33ab9af27f
Legend-parth <parthgajera320@gmail.com>
2025-01-16 17:24:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ mkdir -p ~/.vnc
 echo "yourpassword" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
-# Start x11vnc and websockify
+# Start x11vnc
 x11vnc -display :0 -forever -shared -rfbport 5900 -passwdfile ~/.vnc/passwd -bg
+
+# Start noVNC
 websockify --web /usr/share/novnc 6080 localhost:5900
