c57df524e441003717c482e1bee5f867a6c18c69
Legend-parth <parthgajera320@gmail.com>
2025-01-17 14:44:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# Set a VNC password
-mkdir -p ~/.vnc
-echo "yourpassword" | vncpasswd -f > ~/.vnc/passwd
-chmod 600 ~/.vnc/passwd
+# Start noVNC server
+/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 &
 
-# Start x11vnc
-x11vnc -display :0 -forever -shared -rfbport 5900 -passwdfile ~/.vnc/passwd -bg
-
-# Start noVNC
-websockify --web /usr/share/novnc 6080 localhost:5900
+# Start x11vnc server with pre-configured password
+x11vnc -forever -usepw -create
