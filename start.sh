4fc7be2555f5645bdbb12f2c4ec07e39b646fa84
Legend-parth <parthgajera320@gmail.com>
2025-01-17 17:12:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Set the VNC password non-interactively
+# Set the VNC password
 VNC_PASSWORD_FILE="/root/.vnc/passwd"
 if [ ! -f "$VNC_PASSWORD_FILE" ]; then
     mkdir -p /root/.vnc
-    echo "your_vnc_password" | x11vnc -storepasswd - $VNC_PASSWORD_FILE
+    echo "Legends@2025" | x11vnc -storepasswd - $VNC_PASSWORD_FILE
     chmod 600 $VNC_PASSWORD_FILE
 fi
 
@@ -14,8 +14,10 @@ Xvfb :0 -screen 0 1280x1024x24 &
 # Start the x11vnc server
 x11vnc -forever -usepw -shared -display :0 &
 
-# Start the noVNC web interface
-/opt/noVNC/utils/launch.sh --vnc localhost:5900 --listen 6080 &
+# Start noVNC server
+/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 6080 &
 
+# Start ttyd for terminal access
+ttyd -p 6080 bash &
 # Keep the container running
 tail -f /dev/null
