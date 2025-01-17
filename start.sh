21ae121eaa25736e39da41b6e84b34f904ee0118
Legend-parth <parthgajera320@gmail.com>
2025-01-17 15:51:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Start noVNC server
-/opt/noVNC/utils/novnc_proxy --vnc localhost:5900 &
+# Start noVNC server pointing to VNC server
+/opt/novnc/utils/novnc_proxy --vnc localhost:5900 &
 
 # Start x11vnc server with pre-configured password
-
 x11vnc -forever -usepw -create
